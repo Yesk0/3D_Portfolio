57029a1669059517bd1e99307e1d0c49d6b58c86
Yesko <iskoaleksandor@gmail.com>
2025-06-19 23:46:13 +0500

    chore: initialisation of the project

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+
+const InfoBox = ({ text, link, btnText }) => (
+  <div className="info-box">
+    <p className="font-medium sm:text-xl text-center">{text}</p>
+    <Link to={link} className="neo-brutalism-white neo-btn">
+      {btnText}
+      <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+    </Link>
+  </div>
+);
+
+const renderContent = {
+  1: (
+    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+      Hi, I'm <span className="font-semibold">Yeskendir</span>👋
+      <br />A Software Engineer from Kazakhstan
+    </h1>
+  ),
+  2: (
+    <InfoBox
+      text="Build a solid foundation in various areas through courses, practice, and independent projects"
+      link="/about"
+      btnText="Learn more"
+    />
+  ),
+  3: (
+    <InfoBox
+      text="Worked on various solo projects to apply skills in real scenarios and track measurable progress"
+      link="/projects"
+      btnText="Visit my portfolio"
+    />
+  ),
+  4: (
+    <InfoBox
+      text="Need a project done or looking for a dev? I'm just a few keystrokes way"
+      link="/contact"
+      btnText="Lets talk"
+    />
+  ),
+};
+
+const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 1)
+    return (
+      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+        Hi, I'm <span className="font-semibold">Yeskendir</span>👋
+        <br />A Software Engineer from Kazakhstan
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">
+          Build a solid foundation <br /> in various areas through courses,
+          practice, and independent projects
+        </p>
+
+        <Link to="/about" className="neo-brutalism-white neo-btn">
+          Learn more
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className="info-box">
+        <p className="font-medium text-center sm:text-xl">
+          Worked on various solo <br /> projects to apply skills in real
+          scenarios and track measurable progress
+        </p>
+
+        <Link to="/projects" className="neo-brutalism-white neo-btn">
+          Visit my portfolio
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">
+          Need a project done or looking for a dev? <br /> I'm just a few
+          keystrokes away
+        </p>
+
+        <Link to="/contact" className="neo-brutalism-white neo-btn">
+          Let's talk
+          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
+        </Link>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default HomeInfo;
